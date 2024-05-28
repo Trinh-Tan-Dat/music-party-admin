317f9d634b0e44eb2caae59b2f20f3a70495fba1
ThanhDong00 <thanhdonganmy@gmail.com>
2024-05-29 00:22:05 +0700

    update: UI color

--- src/components/contents/Dashboard.jsx
+++ src/components/contents/Dashboard.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
-import ButtonDashboard from '../controls/buttons/ButtonDashboard'
-import BarChart from '../controls/charts/BarChart'
-import PieChart from '../controls/charts/PieChart'
-import { DASHBOARD_BUTTON_LINKS } from 'components/consts/DashboardButton'
+import React from "react";
+import ButtonDashboard from "../controls/buttons/ButtonDashboard";
+import BarChart from "../controls/charts/BarChart";
+import PieChart from "../controls/charts/PieChart";
+import { DASHBOARD_BUTTON_LINKS } from "components/consts/DashboardButton";
 
 const Dashboard = () => {
-    const classTitle = 'text-xl font-bold ml-1 py-5'
-    const labelsBarChart = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const classTitle = "text-xl font-bold ml-1 py-5 text-gray-400";
+    const labelsBarChart = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
     const dataBarChart = [22, 19, 57, 5, 2, 3, 20, 12, 55, 60, 12, 25];
 
-    const labelPieChart = ['Male', 'Female', 'Others'];
+    const labelPieChart = ["Male", "Female", "Others"];
     const dataPieChart = [57, 29, 17];
 
     return (
-        <div className='flex-shrink max-w-full w-full '>
+        <div className="flex-shrink max-w-full w-full ">
             <div className={classTitle}>Statistic</div>
-            <div className='flex items-center justify-between gap-10'>
-                {DASHBOARD_BUTTON_LINKS.map((link, index) => <ButtonDashboard  key={index} link={link}/>)}
+            <div className="flex items-center justify-between gap-10">
+                {DASHBOARD_BUTTON_LINKS.map((link, index) => (
+                    <ButtonDashboard key={index} link={link} />
+                ))}
             </div>
             <div className={`${classTitle} pt-0`}>Charts</div>
-            <div className='2xl:flex 2xl:flex-row 2xl:items-center 2xl:justify-between 2xl:gap-12'>
+            <div className="2xl:flex 2xl:flex-row 2xl:items-center 2xl:justify-between 2xl:gap-12">
                 <BarChart data={dataBarChart} labels={labelsBarChart} />
                 <PieChart data={dataPieChart} labels={labelPieChart} />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Dashboard
+export default Dashboard;
